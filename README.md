0772ef93543f10b50f743a6922ab88cd07bee28b
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2020-07-29 16:32:03 -0400

    modified:   transbot.py

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A bunch of simple discord bots.
 
 
-transbot.py : translates messages from other languages to english, uses an older version of discord python api wrapper.
+transbot.py : translates messages from other languages to english.
 
 
-makaut_notice.py : checks http://makautexam.net for any changes and sends new notices to the specified discord channel. Uses the newer version of discord python api wrapper.
+makaut_notice.py : checks http://makautexam.net for any changes and sends new notices to the specified discord channel.
--- transbot.py
+++ transbot.py
@@ -1,22 +1,94 @@
-
-import discord
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from googletrans import Translator
+import discord
+from random import randint
+import re
+import requests, uuid
+#===============================================================================================#
 client = discord.Client()
 translator = Translator()
-token='Insert token here'
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
+#===============================================================================================#
 @client.event
 async def on_message(message):
-    if(message.author!=client.user):
-        t=translator.detect(message.content)
-        d=translator.translate(message.content)
-        print(t)
-        print(d)
-        if('en'!=t.lang):
-            await client.send_message(message.channel,"Translation : "+d.text)
- 
+    if (message.author == client.user):
+        return
+    #===========================================================================================#
+    authenticator = IAMAuthenticator('<api key>')
+    language_translator = LanguageTranslatorV3(
+    version='<version>',
+    authenticator=authenticator)
+    language_translator.set_service_url('<service url>')
+    #===========================================================================================#
+    subscription_key = '<api key>'
+    endpoint = '<service url>'
+    pathtranslate = '<translate path>'
+    pathtransliterate = '<transliterate path>'
+    pathdetect = '<detect language path>'
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        'Ocp-Apim-Subscription-Region': '<location>',
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())}
+    #===========================================================================================#
+    print('===========================================================================================')
+    t=str(message.content)
+    print('Message :',t)
+    x=re.findall(r'<@!?[0-9]+>',t)#find mentions
+    y=re.findall(r'<A?:\w+:[0-9]+>',t)#find reactions, including animated ones
+    z=re.findall(r'<#[0-9]+>',t)#find channel mentions
+    print('mentions :',x)
+    print('reactions :',y)
+    print('channel mentions :',z)
+    if(len(x)>0):
+        for i in x: t=t.replace(i,"@")
+    if(len(y)>0):
+        for i in y: t=t.replace(i,":")
+    if(len(z)>0):
+        for i in z: t=t.replace(i,"#")
+    print('Sanitized Message :',t)
+    try:
+        body = [{'text': t}]
+        constructed_url = endpoint + pathdetect
+        request = requests.post(constructed_url, headers=headers, json=body)
+        response = request.json()
+        print('Microsoft Translator Detection Data :')
+        print('Language :',response[0]['language'])
+        print('Score :',response[0]['score'])
+        if response[0]['score'] < 0.6 or 'en' in response[0]['language']:
+            return
+        l=response[0]['language']
+    except Exception as e:
+        print('Microsoft Translator generated an Error, using Google Translator instead :\n',str(e))
+        l = translator.detect(t)
+        print('Google Translator Detection Data :',l)
+        if l.confidence < 0.6 or 'en' in str(l.lang):
+            return
+        l=l.lang
+        pass
+    #===========================================================================================#
+    try:
+        constructed_url = endpoint+pathtranslate+'&to=en'
+        request = requests.post(constructed_url, headers=headers, json=body)
+        response = request.json()
+        tr=(response[0]['translations'][0]['text'])
+    except Exception as z:
+        print('Microsoft Translator generated an Error, using IBM Watson instead :\n',str(z))
+        translation = language_translator.translate(text=t,model_id=l+'-en').get_result()
+        tr=(dict(json.loads(json.dumps(translation, indent=2, ensure_ascii=False)).items())['translations'][0]['translation'])
+    except Exception as e:
+        print('IBM Watson generated an Error, using Google Translator instead :\n',str(e))
+        tr=translator.translate(t).text
+        pass
+    print('Translated Text :',tr)
+    if(str(tr).lower().strip()==str(t).lower().strip()):
+        return
+    await message.channel.send("Translation : " + tr)
+    print('===========================================================================================')
 
-client.run(token,bot=False)# you have to set bot=True if you are using a bot account
+@client.event
+async def on_ready():
+    print("Logged In")
+    
+client.run('<bot token>',bot=True)
